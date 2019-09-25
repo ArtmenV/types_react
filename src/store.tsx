a822d7f3f8c76e044a9e39ca207e5cd4f8621d09
Artmen_V <viliutich.artem@gmail.com>
2019-09-25 16:49:08 +0700

    fetch_data

--- src/store.tsx
+++ src/store.tsx
@@ -5,15 +5,32 @@ interface IState {
   favorites: [];
 }
 
+interface IAction {
+  type: string;
+  payload: any;
+}
+
 const initialState: IState = {
   episodes: [],
   favorites: []
 };
 
-export const Store = React.createContext<IState>(initialState);
+export const Store = React.createContext<IState | any>(initialState);
 
-function reducer() {}
+function reducer(state: IState, action: IAction): IState {
+  switch (action.type) {
+    case "FETCH_DATA":
+      return { ...state, episodes: action.payload };
+    default:
+      return state;
+  }
+}
 
 export function StoreProvider(props: any) {
-  return <Store.Provider value={initialState}>{props.children}</Store.Provider>;
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  return (
+    <Store.Provider value={{ state, dispatch }}>
+      {props.children}
+    </Store.Provider>
+  );
 }
