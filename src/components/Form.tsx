4b82f0f81756a7cfad631c6dbc429427ffb8e1b9
Artmen_V <viliutich.artem@gmail.com>
2019-09-25 13:21:08 +0700

    type_todos

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -17,8 +17,20 @@ export const Form: React.FC = () => {
     setValue("");
   };
 
-  const addTodo = (text: string) => {
+  const addTodo = (text: string): void => {
     const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos.splice(index, 1);
     setTodos(newTodos);
   };
 
@@ -31,13 +43,26 @@ export const Form: React.FC = () => {
           type="text"
           placeholder="input text"
           onChange={e => setValue(e.target.value)}
+          required
         />
         <button type="submit">Add Todo</button>
       </form>
       <section>
-        {todos.map((todo: ITodo, index: number) => {
-          return <div key={index}>{todo.text}</div>;
-        })}
+        {todos.map((todo: ITodo, index: number) => (
+          <React.Fragment key={index}>
+            <div
+              style={{ textDecoration: todo.complete ? "line-through" : "" }}
+            >
+              {todo.text}
+            </div>
+            <button type="button" onClick={() => completeTodo(index)}>
+              {todo.complete ? "imcomplete" : "Complete"}
+            </button>
+            <button type="button" onClick={() => deleteTodo(index)}>
+              &times;
+            </button>
+          </React.Fragment>
+        ))}
       </section>
     </>
   );
