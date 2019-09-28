c59505d71c88448bfe3db0c5e04db331334a4484
Artmen_V <viliutich.artem@gmail.com>
2019-09-28 17:08:03 +0700

    add_item_fix_bug

---
+++ src/components/Note-item.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { AppState } from "../store/index";
+import styles from "./Notes.module.css";
+import { deleteAction } from "../store/InputField/action";
+import { Note } from "../store/InputField/types";
+
+// interface NotesProps {
+//   text: string,
+//   id: number,
+//   important: boolean,
+//   deleteNotes: () => : void
+// }
+export const NoteItem = ({ text, deleteNotes, id }: any) => {
+  return (
+    <li className={styles.notes__list_item}>
+      {text}
+      <div className="notes__list_group_button">
+        <button
+          className={styles.notes__list_button_important}
+          // onClick={() => addImportantAction(note.id)}
+        >
+          !
+        </button>
+        <button
+          type="button"
+          className={styles.notes__list_button}
+          onClick={() => deleteNotes(id)}
+        >
+          &times;
+        </button>
+      </div>
+    </li>
+  );
+};
--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -4,6 +4,7 @@ import { AppState } from "../store/index";
 import styles from "./Notes.module.css";
 import { deleteAction } from "../store/InputField/action";
 import { Note } from "../store/InputField/types";
+import { NoteItem } from "./Note-item";
 
 // interface NotesProps {
 //   notes: [];
@@ -19,28 +20,61 @@ export const Notel = () => {
 
   return (
     <ul className={styles.notes__list}>
-      {notes.map((note: Note) => {
-        return (
-          <li key={note.id} className={styles.notes__list_item}>
-            {note.text}
-            <div className="notes__list_group_button">
-              <button
-                className={styles.notes__list_button_important}
-                // onClick={() => addImportantAction(note.id)}
-              >
-                !
-              </button>
-              <button
-                type="button"
-                className={styles.notes__list_button}
-                onClick={() => deleteNotes(note.id)}
-              >
-                &times;
-              </button>
-            </div>
-          </li>
-        );
-      })}
+      {notes.map(note => (
+        <NoteItem
+          key={note.id}
+          text={note.text}
+          deleteNotes={deleteNotes}
+          id={note.id}
+        />
+      ))}
     </ul>
   );
 };
+
+// import React from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { AppState } from "../store/index";
+// import styles from "./Notes.module.css";
+// import { deleteAction } from "../store/InputField/action";
+// import { Note } from "../store/InputField/types";
+
+// // interface NotesProps {
+// //   notes: [];
+// // }
+// export const Notel = () => {
+//   const notes = useSelector((state: AppState) => state.NotesReducer.notes);
+//   console.log(notes);
+//   const dispatch = useDispatch();
+
+//   const deleteNotes = (id: any) => {
+//     dispatch(deleteAction(id));
+//   };
+
+//   return (
+//     <ul className={styles.notes__list}>
+//       {notes.map((note: Note) => {
+//         return (
+//           <li key={note.id} className={styles.notes__list_item}>
+//             {note.text}
+//             <div className="notes__list_group_button">
+//               <button
+//                 className={styles.notes__list_button_important}
+//                 // onClick={() => addImportantAction(note.id)}
+//               >
+//                 !
+//               </button>
+//               <button
+//                 type="button"
+//                 className={styles.notes__list_button}
+//                 onClick={() => deleteNotes(note.id)}
+//               >
+//                 &times;
+//               </button>
+//             </div>
+//           </li>
+//         );
+//       })}
+//     </ul>
+//   );
+// };
